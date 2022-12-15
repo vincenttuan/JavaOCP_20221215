460f051f34a2106e4c164241781e24c97178f23f
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-15 21:04:23 +0800

    ocp

---
+++ src/main/java/com/ocp/day01_basic/ArrayTest2.java
@@ -0,0 +1,20 @@
+package com.ocp.day01_basic;
+
+import java.util.Arrays;
+
+public class ArrayTest2 {
+    public static void main(String[] args) {
+        int[] scores = {100, 85, 74};
+        System.out.println(scores);
+        System.out.println(Arrays.toString(scores));
+        // 想要計算總分 = ?
+        int sum = 0;
+        for(int x : scores) {
+            sum += x; // sum = sum + x;
+        }
+        System.out.println(sum);
+        // 印出平均
+        double avg = (double)sum / scores.length;
+        System.out.println(avg);
+    }
+}
