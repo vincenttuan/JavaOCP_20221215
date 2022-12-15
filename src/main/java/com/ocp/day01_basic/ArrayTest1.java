b24abab30d32cd2f922351732a571f40d4b3a812
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-15 20:06:49 +0800

    ocp

---
+++ src/main/java/com/ocp/day01_basic/ArrayTest1.java
@@ -0,0 +1,12 @@
+package com.ocp.day01_basic;
+
+public class ArrayTest1 {
+    public static void main(String[] args) {
+        int[] scores = {100, 85, 74};
+               // 位置:  0    1   2
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+        System.out.println(scores.length);
+    }
+}
