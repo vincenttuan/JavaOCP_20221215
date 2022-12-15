8026a670abf4408f8a1be810421e7e05609f3c5b
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-15 20:00:10 +0800

    ocp

--- src/main/java/com/ocp/day01_basic/BMITest1.java
+++ src/main/java/com/ocp/day01_basic/BMITest1.java
@@ -8,19 +8,24 @@ public class BMITest1 {
         double height = 170.0; // 身高
         double weight = 60.0; // 體重
         double bmi = getBMI(height, weight);
-        System.out.printf("%s BMI: %.2f\n", name, bmi);
+        print(name, bmi);
         
         String name2 = "Mary";
         double height2 = 160;
         double weight2 = 55;
         double bmi2 = getBMI(height2, weight2);
-        System.out.printf("%s BMI: %.2f\n", name2, bmi2);
+        print(name2, bmi2);
     }
     
     // 建立一個計算 bmi 值的方法並且在計算完畢後會回傳(return) bmi 值
     public static double getBMI(double h, double w) {
         double bmi = w / Math.pow(h/100, 2);
         return bmi;
+    }
+    
+    // 建立一個列印 name 與 bmi 的方法
+    public static void print(String name, double bmi) {
+        System.out.printf("%s BMI: %.2f\n", name, bmi);
     }
     
     
