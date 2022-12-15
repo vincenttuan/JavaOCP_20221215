3fb2ea130cc3509907cacdf968d0312f45122a9f
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-15 21:41:41 +0800

    ocp

--- src/main/java/com/ocp/day01_basic/ArrayTest4.java
+++ src/main/java/com/ocp/day01_basic/ArrayTest4.java
@@ -1,12 +1,16 @@
 package com.ocp.day01_basic;
 
+import static com.ocp.day01_basic.ArrayTest3.getAvg;
+
 public class ArrayTest4 {
     public static void main(String[] args) {
         int[] cClass = {89, 91, 69}; // A 班
         int[] dClass = {92, 65, 80};  // B 班
         // 試問哪一個班級平均分數較高 ?
-        double cAvg = ArrayTest3.getAvg(cClass);
-        double dAvg = ArrayTest3.getAvg(dClass);
+        //double cAvg = ArrayTest3.getAvg(cClass);
+        //double dAvg = ArrayTest3.getAvg(dClass);
+        double cAvg = getAvg(cClass);
+        double dAvg = getAvg(dClass);
         System.out.printf("C 班平均: %.1f\n", cAvg);
         System.out.printf("D 班平均: %.1f\n", dAvg);
         System.out.println(cAvg > dAvg ? "C 班平均高" : "D 班平均高");
