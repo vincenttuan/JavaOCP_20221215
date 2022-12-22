d807f7cf0f5620baf6bb29b1058a86d8f9369f66
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-22 19:25:59 +0800

    ocp

--- src/main/java/com/ocp/day02_basic/BmiDemo.java
+++ src/main/java/com/ocp/day02_basic/BmiDemo.java
@@ -4,7 +4,6 @@ public class BmiDemo {
     
     public static void main(String[] args) {
         Bmi b1 = new Bmi();
-        Bmi b2 = new Bmi();
         b1.name = "John";
         b1.weight = 60.0;
         b1.height = 170.0;
