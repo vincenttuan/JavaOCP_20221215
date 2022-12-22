6db99208779da3df95bba57467301ccb4a5ecc32
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-22 19:47:15 +0800

    ocp

--- src/main/java/com/ocp/day02_basic/StudentDemo.java
+++ src/main/java/com/ocp/day02_basic/StudentDemo.java
@@ -3,7 +3,13 @@ package com.ocp.day02_basic;
 public class StudentDemo {
     
     public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "John";
+        s1.score = 75;
+        s1.check();
         
+        Student s2 = new Student();
+        s2.print();
         
         
     }
