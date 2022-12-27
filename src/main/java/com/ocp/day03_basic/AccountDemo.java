95285c3971e94744f29baa6b6aee381d23fe2e4b
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-27 19:44:48 +0800

    ocp

--- src/main/java/com/ocp/day03_basic/AccountDemo.java
+++ src/main/java/com/ocp/day03_basic/AccountDemo.java
@@ -4,6 +4,8 @@ public class AccountDemo {
     public static void main(String[] args) {
         Account act1 = new Account();
         Account act2 = new Account();
+        act1.deposit(1000);
+        act2.deposit(800);
         
         System.out.printf("act1 cash: $%d\n", act1.cash);
         System.out.printf("act2 cash: $%d\n", act2.cash);
