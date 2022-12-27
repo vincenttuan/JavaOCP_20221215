4765c23b74055831e836e931c29eb5d4c0dfd7e3
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-27 19:43:03 +0800

    ocp

---
+++ src/main/java/com/ocp/day03_basic/AccountDemo.java
@@ -0,0 +1,12 @@
+package com.ocp.day03_basic;
+
+public class AccountDemo {
+    public static void main(String[] args) {
+        Account act1 = new Account();
+        Account act2 = new Account();
+        
+        System.out.printf("act1 cash: $%d\n", act1.cash);
+        System.out.printf("act2 cash: $%d\n", act2.cash);
+        
+    }
+}
