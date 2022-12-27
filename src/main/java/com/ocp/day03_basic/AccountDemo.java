2cc56a70688934bcb107dc8c714df650c504ad06
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-27 19:52:06 +0800

    ocp

--- src/main/java/com/ocp/day03_basic/AccountDemo.java
+++ src/main/java/com/ocp/day03_basic/AccountDemo.java
@@ -6,7 +6,19 @@ public class AccountDemo {
         Account act2 = new Account();
         act1.deposit(1000);
         act2.deposit(800);
-        
+        System.out.printf("act1 cash: $%d\n", act1.cash);
+        System.out.printf("act2 cash: $%d\n", act2.cash);
+        //---------------------------------------------------------
+        act1.withdraw(300);
+        act2.withdraw(200);
+        System.out.printf("act1 cash: $%d\n", act1.cash);
+        System.out.printf("act2 cash: $%d\n", act2.cash);
+        //---------------------------------------------------------
+        act1.transfer(500, act2);
+        System.out.printf("act1 cash: $%d\n", act1.cash);
+        System.out.printf("act2 cash: $%d\n", act2.cash);
+        //---------------------------------------------------------
+        act2.transfer(400, act1);
         System.out.printf("act1 cash: $%d\n", act1.cash);
         System.out.printf("act2 cash: $%d\n", act2.cash);
         
