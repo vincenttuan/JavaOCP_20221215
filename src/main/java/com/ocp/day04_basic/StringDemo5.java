f2ce11afd1de717e0dff23dddc53900fded43f31
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-29 19:03:34 +0800

    ocp

---
+++ src/main/java/com/ocp/day04_basic/StringDemo5.java
@@ -0,0 +1,12 @@
+package com.ocp.day04_basic;
+
+public class StringDemo5 {
+    public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer("Java");
+        //sb = sb.append("SCJP");
+        sb.append("SCJP");
+        System.out.println(sb);
+        
+    }
+}
