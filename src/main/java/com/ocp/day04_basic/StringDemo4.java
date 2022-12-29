4940fde5ad85c5650d360c5e35576e046b34de36
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-29 18:57:54 +0800

    ocp

---
+++ src/main/java/com/ocp/day04_basic/StringDemo4.java
@@ -0,0 +1,12 @@
+package com.ocp.day04_basic;
+
+public class StringDemo4 {
+    public static void main(String[] args) {
+        
+        String s = "Java";
+        //s = s.concat("SCJP");
+        s.concat("SCJP");
+        System.out.println(s);
+        
+    }
+}
