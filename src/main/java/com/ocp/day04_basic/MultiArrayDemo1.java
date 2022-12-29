1d8173c2c2e79ad5e56d2714fd371cc4ca8278a0
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-29 19:43:20 +0800

    ocp

---
+++ src/main/java/com/ocp/day04_basic/MultiArrayDemo1.java
@@ -0,0 +1,11 @@
+package com.ocp.day04_basic;
+
+public class MultiArrayDemo1 {
+    public static void main(String[] args) {
+        int[][] m = new int[2][3];
+        System.out.println(m.length);
+        System.out.println(m);
+        System.out.println(m[0]);
+        System.out.println(m[0].length);
+    }
+}
