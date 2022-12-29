7ad018041aad5891160902f463fd7a3e9e482f63
MB-teacher <MB-teacher@MB-207-teacher>
2022-12-29 19:57:30 +0800

    ocp

---
+++ src/main/java/com/ocp/day04_basic/MultiArrayDemo3.java
@@ -0,0 +1,15 @@
+package com.ocp.day04_basic;
+
+public class MultiArrayDemo3 {
+    public static void main(String[] args) {
+        int[][] m = new int[2][];
+        m[0] = new int[3];
+        m[1] = new int[1];
+        m[0][0] = 100;
+        m[0][1] = 90;
+        m[0][2] = 80;
+        m[1][0] = 70;
+        // 請逐筆將陣列元素資料印出
+        
+    }
+}
