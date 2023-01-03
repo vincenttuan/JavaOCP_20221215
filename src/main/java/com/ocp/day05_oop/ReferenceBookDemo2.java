7d35e200572fd6c1d32bc0b55457c6d3ad41c568
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 21:20:17 +0800

    ocp

--- src/main/java/com/ocp/day05_oop/ReferenceBookDemo2.java
+++ src/main/java/com/ocp/day05_oop/ReferenceBookDemo2.java
@@ -4,11 +4,17 @@ public class ReferenceBookDemo2 {
     public static void main(String[] args) {
         Author author = new Author("Mary", 40);
         System.out.println(author);
-        ReferenceBook book = new ReferenceBook("Python", 350, 250, author);
-        System.out.println(book);
+        ReferenceBook book1 = new ReferenceBook("Python", 350, 250, author);
+        System.out.println(book1);
         
         ReferenceBook book2 = new ReferenceBook("VB", 250, 200, new Author("Bob", 28));
         System.out.println(book2);
+        
+        ReferenceBook book3 = new ReferenceBook("C#", 450, 600, new Author("Jack", 46));
+        System.out.println(book3);
+        
+        ReferenceBook book4 = new ReferenceBook("Java", 550, 620, author);
+        System.out.println(book4);
         
     }
 }
