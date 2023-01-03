fddcd9a58f3b53edd5059097dbee971fd1025e1e
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 19:17:51 +0800

    ocp

--- src/main/java/com/ocp/day05_oop/Book.java
+++ src/main/java/com/ocp/day05_oop/Book.java
@@ -3,11 +3,29 @@ package com.ocp.day05_oop;
 // 描述一本書即有那些資料?
 public class Book {
     String title; // 名稱
-    int price; // 價格
-    int pages; // 頁數
+    private int price = 200; // 價格
+    private int pages = 1; // 頁數
     String authorName; // 作者姓名
-    int authorAge; // 作者年齡
-
+    private int authorAge = 18; // 作者年齡
+    
+    public void setPrice(int price) {
+        if(price > 200) {
+            this.price = price;
+        }
+    }
+    
+    public void setPages(int pages) {
+        if(pages > 1) {
+            this.pages = pages;
+        }
+    }
+    
+    public void setAuthorAge(int age) {
+        if(age > 18) {
+            this.authorAge = age;
+        }
+    }
+    
     @Override
     public String toString() {
         return "Book{" + "title=" + title + ", price=" + price + ", pages=" + pages + ", authorName=" + authorName + ", authorAge=" + authorAge + '}';
--- src/main/java/com/ocp/day05_oop/BookDemo.java
+++ src/main/java/com/ocp/day05_oop/BookDemo.java
@@ -4,10 +4,10 @@ public class BookDemo {
     public static void main(String[] args) {
         Book b1 = new Book();
         b1.title = "Java";
-        b1.price = 500;
-        b1.pages = 1200;
+        b1.setPrice(201);
+        b1.setPages(750);
         b1.authorName = "John";
-        b1.authorAge = 35;
-        System.out.println(b1);
+        b1.setAuthorAge(35);
+        System.out.println(b1.toString());
     }
 }
