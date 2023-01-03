a722ab7f15dd809e6b0aac8488b1b5f5d489353c
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 19:05:07 +0800

    ocp

--- src/main/java/com/ocp/day05_oop/Book.java
+++ src/main/java/com/ocp/day05_oop/Book.java
@@ -3,10 +3,17 @@ package com.ocp.day05_oop;
 // 描述一本書即有那些資料?
 public class Book {
     String title; // 名稱
-    int pricel; // 價格
+    int price; // 價格
     int pages; // 頁數
     String authorName; // 作者姓名
     int authorAge; // 作者年齡
+
+    @Override
+    public String toString() {
+        return "Book{" + "title=" + title + ", price=" + price + ", pages=" + pages + ", authorName=" + authorName + ", authorAge=" + authorAge + '}';
+    }
+    
+    
 }
 
 
---
+++ src/main/java/com/ocp/day05_oop/BookDemo.java
@@ -0,0 +1,12 @@
+package com.ocp.day05_oop;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        b1.title = "Java";
+        b1.price = 500;
+        b1.pages = 1200;
+        b1.authorName = "John";
+        b1.authorAge = 35;
+    }
+}
