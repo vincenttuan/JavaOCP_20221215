b8a20f0004b11a94a2b2595670640869d3cd660c
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 19:01:09 +0800

    ocp

---
+++ src/main/java/com/ocp/day05_oop/Book.java
@@ -0,0 +1,12 @@
+package com.ocp.day05_oop;
+
+// 描述一本書即有那些資料?
+public class Book {
+    String title; // 名稱
+    int pricel; // 價格
+    int pages; // 頁數
+    String authorName; // 作者姓名
+    int authorAge; // 作者年齡
+}
+
+
