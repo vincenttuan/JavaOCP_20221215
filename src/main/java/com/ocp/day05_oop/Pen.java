a1e4a87d97669a4d7431a7d46f20d4cd83f7c0a1
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 19:37:02 +0800

    ocp

--- src/main/java/com/ocp/day05_oop/Pen.java
+++ src/main/java/com/ocp/day05_oop/Pen.java
@@ -1,9 +1,25 @@
 package com.ocp.day05_oop;
 
-// Pen 的 color 只能是 red, blue, black
-// Pen 的 price 必須介於 10~20 之間
+// Pen 的 color (預設是 blue) 只能是 red, blue, black
+// Pen 的 price (預設是 15) 必須介於 10~20 之間
 public class Pen {
-    String color;
-    int price;
+    private String color = "blue";
+    private int price = 15;
+    
+    public void setColor(String color) {
+        color = color.toLowerCase(); // 全部轉小寫
+        /*
+        if(color.equals("red") || color.equals("blue") || color.equals("black")) {
+            this.color = color;
+        }
+        */
+        switch (color) {
+            case "red":
+            case "blue":
+            case "black":
+                this.color = color;
+        }
+    }
+    
     
 }
