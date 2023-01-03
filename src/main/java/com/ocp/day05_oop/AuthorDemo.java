a8ae0cd0eb4828797f9414cbe3f8ac079ca9ba9d
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 19:58:44 +0800

    ocp

---
+++ src/main/java/com/ocp/day05_oop/AuthorDemo.java
@@ -0,0 +1,12 @@
+package com.ocp.day05_oop;
+
+public class AuthorDemo {
+    public static void main(String[] args) {
+        Author author = new Author();
+        author.setName("John");
+        author.setAge(35);
+        System.out.println(author);
+        System.out.println(author.getName());
+        System.out.println(author.getAge());
+    }
+}
