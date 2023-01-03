4cafe1773f04215b2b278161a99644dfeae32482
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-03 20:34:23 +0800

    ocp

--- src/main/java/com/ocp/day05_oop/ReferenceBook.java
+++ src/main/java/com/ocp/day05_oop/ReferenceBook.java
@@ -5,5 +5,46 @@ public class ReferenceBook {
     private int price; // 價格
     private int pages; // 頁數
     private Author author;  // 作者
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return "ReferenceBook{" + "title=" + title + ", price=" + price + ", pages=" + pages + ", author=" + author + '}';
+    }
+    
+    
     
 }
