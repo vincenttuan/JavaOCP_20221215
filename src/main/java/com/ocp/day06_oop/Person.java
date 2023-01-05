e92e4cd40d75c58a9af4d94e78cfcc1c28a48756
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-05 20:35:21 +0800

    ocp

--- src/main/java/com/ocp/day06_oop/Person.java
+++ src/main/java/com/ocp/day06_oop/Person.java
@@ -3,7 +3,16 @@ package com.ocp.day06_oop;
 public class Person {
     private String name;
     private int age;
+    
+    public Person() {
+    
+    }
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
     public String getName() {
         return name;
     }
---
+++ src/main/java/com/ocp/day06_oop/PersonDemo3.java
@@ -0,0 +1,8 @@
+package com.ocp.day06_oop;
+
+public class PersonDemo3 {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        
+    }
+}
