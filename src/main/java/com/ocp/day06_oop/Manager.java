ca6c6319199593ef2349be4dd6c6cc07ced041a4
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-05 20:32:23 +0800

    ocp

--- src/main/java/com/ocp/day06_oop/Manager.java
+++ src/main/java/com/ocp/day06_oop/Manager.java
@@ -2,7 +2,20 @@ package com.ocp.day06_oop;
 
 public class Manager extends Employee {
     private int budget;
-
+    
+    public Manager() {
+    }
+    
+    public Manager(String name, int age, int salary, int budget) {
+        super(name, age, salary);
+        /*
+        setName(name);
+        setAge(age);
+        setSalary(salary);
+        */
+        this.budget = budget;
+    }
+    
     public int getBudget() {
         return budget;
     }
@@ -10,5 +23,12 @@ public class Manager extends Employee {
     public void setBudget(int budget) {
         this.budget = budget;
     }
+
+    @Override
+    public String toString() {
+        return "Manager{" + "budget=" + budget + "} " + super.toString();
+    }
+    
+    
     
 }
