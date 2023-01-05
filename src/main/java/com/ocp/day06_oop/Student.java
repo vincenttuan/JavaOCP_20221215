5b77ade0c71d6255c9be6ae936904ee6a9fe7809
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-05 19:50:48 +0800

    ocp

--- src/main/java/com/ocp/day06_oop/Student.java
+++ src/main/java/com/ocp/day06_oop/Student.java
@@ -23,7 +23,7 @@ public class Student extends Person {
 
     @Override
     public String toString() {
-        return "Student{" + "score=" + score + '}';
+        return "Student{" + "score=" + score + "} " + super.toString();
     }
 
     
