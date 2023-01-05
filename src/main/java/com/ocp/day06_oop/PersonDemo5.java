a097331175b6f5e33043beab3db115068bbc7185
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-05 21:26:55 +0800

    ocp

---
+++ src/main/java/com/ocp/day06_oop/PersonDemo5.java
@@ -0,0 +1,20 @@
+package com.ocp.day06_oop;
+
+public class PersonDemo5 {
+    public static void main(String[] args) {
+        Student s1 = new Student("John", 18, 90);
+        Student s2 = new Student("Mary", 18, 55);
+        Student s3 = new Student("Helen", 19, 45);
+        Student s4 = new Student("Jane", 17, 80);
+        Student s5 = new Student("Kitty", 18, 100);
+        
+        Student[] students = {s1, s2, s3, s4, s5};
+        // 請用 傳統 Java 印出及格的學生名字(只要印出名字就好)
+        
+        
+        // 請用 Java Stream 印出及格的學生名字(只要印出名字就好)
+        
+        
+        
+    }
+}
