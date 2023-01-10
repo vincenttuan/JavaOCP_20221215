00c60fdaafe59faa29932359e91ab236eb2ba3b7
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-10 19:20:33 +0800

    ocp

---
+++ src/main/java/com/ocp/day07_oop/EmpAndMgrDemo.java
@@ -0,0 +1,10 @@
+package com.ocp.day07_oop;
+
+public class EmpAndMgrDemo {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.job();
+        Manager manager = new Manager();
+        manager.job();
+    }
+}
--- src/main/java/com/ocp/day07_oop/Manager.java
+++ src/main/java/com/ocp/day07_oop/Manager.java
@@ -1,5 +1,13 @@
 package com.ocp.day07_oop;
 
 public class Manager extends Employee {
+    @Override // 覆寫註解(檢查覆寫是否合法)
+    public void job() {
+        System.out.println("管理");
+    }
+    
+    public void report() { // 一般方法
+        System.out.println("寫報告");
+    }
     
 }
