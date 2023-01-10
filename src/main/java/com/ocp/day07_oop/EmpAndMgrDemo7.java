f09535174a0d3c1d18ad897792c2788054e76c37
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-10 21:24:19 +0800

    ocp

--- src/main/java/com/ocp/day07_oop/EmpAndMgrDemo7.java
+++ src/main/java/com/ocp/day07_oop/EmpAndMgrDemo7.java
@@ -12,7 +12,23 @@ public class EmpAndMgrDemo7 {
         };
         
         // 請問非 Manager 的 Employee 總薪資為何 ?
-        
-        
+        int sum = 0;
+        for(Employee emp : employees) {
+            if(emp instanceof Manager) {
+                // 不處理
+            } else {
+                // 處理
+                sum += emp.getSalary();
+            }
+        }
+        System.out.println(sum);
+        //---------------------------------
+        int sum2 = 0;
+        for(Employee emp : employees) {
+            if(!(emp instanceof Manager)) {
+                sum2 += emp.getSalary();
+            }
+        }
+        System.out.println(sum2);
     }
 }
