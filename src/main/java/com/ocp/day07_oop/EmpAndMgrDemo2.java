d1dac8fce314b34c7b2c349da4d8d4f4cc1fa9c5
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-10 19:45:59 +0800

    ocp

--- src/main/java/com/ocp/day07_oop/EmpAndMgrDemo2.java
+++ src/main/java/com/ocp/day07_oop/EmpAndMgrDemo2.java
@@ -2,7 +2,16 @@ package com.ocp.day07_oop;
 
 public class EmpAndMgrDemo2 {
     public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.job();
+        if(emp1 instanceof Manager) {
+            ((Manager)emp1).report();
+        }
         
-        
+        Employee emp2 = new Manager();
+        emp2.job();
+        if(emp2 instanceof Manager) {
+            ((Manager)emp2).report();
+        }
     }
 }
