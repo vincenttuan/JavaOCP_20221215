72b28115b8fb4450c7ae4cd724399b2819f266b6
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-10 21:08:31 +0800

    ocp

---
+++ src/main/java/com/ocp/day07_oop/EmpAndMgrDemo7.java
@@ -0,0 +1,18 @@
+package com.ocp.day07_oop;
+
+public class EmpAndMgrDemo7 {
+    public static void main(String[] args) {
+        Employee[] employees = {
+            new Employee(30000),
+            new Employee(40000),
+            new Manager(80000),
+            new Employee(45000),
+            new Manager(90000),
+            new Employee(50000),
+        };
+        
+        // 請問非 Manager 的 Employee 總薪資為何 ?
+        
+        
+    }
+}
