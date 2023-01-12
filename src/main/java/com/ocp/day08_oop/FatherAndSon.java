ca5952595be0b4491aced924589dfddcb562938f
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-12 20:57:04 +0800

    ocp

--- src/main/java/com/ocp/day08_oop/FatherAndSon.java
+++ src/main/java/com/ocp/day08_oop/FatherAndSon.java
@@ -1,26 +1,28 @@
 package com.ocp.day08_oop;
 
 class Father {
-
     Father() {
         System.out.println("A"); 
     }
-
 }
 
 class Son extends Father {
-
     Son() {
+        this(1.5, 2.5);
         System.out.println("B");
     }
-    
-    Son(int x) {
+    Son(int x, int y) {
         this();
+        System.out.println("D");
+    }
+    Son(double x, double y) {
+        System.out.println("E");
+    }
+    Son(int x) {
+        this(10, 20);
         System.out.println("C");
     }
-    
 }
-
 
 public class FatherAndSon {
     public static void main(String[] args) {
