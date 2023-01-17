f75b706f6cc2a8db8535cf603a65fa2b04c1d6a0
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-17 20:54:11 +0800

    ocp

--- src/main/java/com/ocp/day09_oop2/Student.java
+++ src/main/java/com/ocp/day09_oop2/Student.java
@@ -1,20 +1,33 @@
 package com.ocp.day09_oop2;
 
 public class Student extends SchoolPerson {
+    private int score;
+    private String name;
+    private int age;
+    
+    public Student() {
+    
+    }
 
+    public Student(int score, String name, int age) {
+        this.score = score;
+        this.name = name;
+        this.age = age;
+    }
+    
     @Override
     public int getScore() {
-        return 90;
+        return score;
     }
 
     @Override
     public String getName() {
-        return "John";
+        return name;
     }
 
     @Override
     public int getAge() {
-        return 18;
+        return age;
     }
     
 }
