9475442c44d0a792cf8290e1c7dbf34a4bc44463
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-17 21:31:43 +0800

    ocp

--- src/main/java/com/ocp/day09_oop2/BusinessPerson.java
+++ src/main/java/com/ocp/day09_oop2/BusinessPerson.java
@@ -1,5 +1,31 @@
 package com.ocp.day09_oop2;
 
 public abstract class BusinessPerson implements Person {
-    public abstract int getSalary();
+    private int salary;
+    private String name;
+    private int age;
+
+    public BusinessPerson() {
+    }
+    
+    public BusinessPerson(int salary, String name, int age) {
+        this.salary = salary;
+        this.name = name;
+        this.age = age;
+    }
+    
+    //public abstract int getSalary();
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
 }
--- src/main/java/com/ocp/day09_oop2/Teacher.java
+++ src/main/java/com/ocp/day09_oop2/Teacher.java
@@ -1,32 +1,9 @@
 package com.ocp.day09_oop2;
 
 public class Teacher extends BusinessPerson {
-    private int salary;
-    private String name;
-    private int age;
-
     public Teacher() {
     }
-
     public Teacher(int salary, String name, int age) {
-        this.salary = salary;
-        this.name = name;
-        this.age = age;
+        super(salary, name, age);
     }
-    
-    @Override
-    public int getSalary() {
-        return salary;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public int getAge() {
-        return age;
-    }
-    
 }
