d230f90cab5c8c9cab94e3f06d8d041ce949f835
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-19 21:15:16 +0800

    ocp

--- src/main/java/com/ocp/day10_employees/EmployeeMain.java
+++ src/main/java/com/ocp/day10_employees/EmployeeMain.java
@@ -1,5 +1,43 @@
 package com.ocp.day10_employees;
 
+import java.util.Scanner;
+
 public class EmployeeMain {
+    private static boolean play = true;
+    public static void main(String[] args) {
+        while (play) {            
+            menu();
+        }
+        System.out.println("系統結束");
+    }
     
+    public static void menu() {
+        System.out.println("-------------------");
+        System.out.println("1. 新增員工");
+        System.out.println("2. 查詢所有員工");
+        System.out.println("3. 修改員工薪資");
+        System.out.println("4. 刪除員工");
+        System.out.println("0. Exit");
+        System.out.println("-------------------");
+        action();
+    }
+    
+    public static void action() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請選擇 ==> ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1: // 新增員工
+                break;
+            case 2: // 查詢所有員工
+                break;
+            case 3: // 修改員工薪資
+                break;
+            case 4: // 刪除員工
+                break;
+            case 0: // Exit
+                play = false;
+                break;
+        }
+    }
 }
