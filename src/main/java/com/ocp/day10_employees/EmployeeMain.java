f12ed038d52b7e7a583165b1561da08e10109546
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-19 21:21:28 +0800

    ocp

--- src/main/java/com/ocp/day10_employees/EmployeeMain.java
+++ src/main/java/com/ocp/day10_employees/EmployeeMain.java
@@ -1,9 +1,14 @@
 package com.ocp.day10_employees;
 
+import com.ocp.day10_employees.entity.Employee;
+import com.ocp.day10_employees.service.EmployeeService;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeeMain {
     private static boolean play = true;
+    private static EmployeeService service = new EmployeeService();
+    
     public static void main(String[] args) {
         while (play) {            
             menu();
@@ -28,8 +33,10 @@ public class EmployeeMain {
         int choice = scanner.nextInt();
         switch (choice) {
             case 1: // 新增員工
+                addEmployee();
                 break;
             case 2: // 查詢所有員工
+                printAllEmployees();
                 break;
             case 3: // 修改員工薪資
                 break;
@@ -40,4 +47,24 @@ public class EmployeeMain {
                 break;
         }
     }
+    
+    public static void addEmployee() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("請輸入: 員工姓名 出生年-月-日 薪資");
+        String name = scanner.next();
+        String birth = scanner.next();
+        Integer salary = scanner.nextInt();
+        service.addEmployee(name, birth, salary);
+    }
+    
+    public static void printAllEmployees() {
+        ArrayList<Employee> employees = service.findAll();
+        System.out.println("-----------------------------");
+        for (Employee e : employees) {
+            System.out.printf("%s %s %d\n", 
+                    e.getName(), e.getBirth(), e.getSalary());
+        }
+        System.out.println("-----------------------------");
+    }
+    
 }
