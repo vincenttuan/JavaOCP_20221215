bb7762a3a1a00e8a6d72608873a7c1a727732b46
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-19 19:42:00 +0800

    ocp

--- src/main/java/com/ocp/day10_employees/entity/Employee.java
+++ src/main/java/com/ocp/day10_employees/entity/Employee.java
@@ -1,5 +1,40 @@
 package com.ocp.day10_employees.entity;
 
+import java.util.Date;
+
 public class Employee {
+    private String name;
+    private Date birth;
+    private Integer salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name=" + name + ", birth=" + birth + ", salary=" + salary + '}';
+    }
+    
     
 }
