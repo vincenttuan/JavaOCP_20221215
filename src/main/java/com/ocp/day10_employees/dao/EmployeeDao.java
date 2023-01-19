a904ba96c213ffc9139a5cb1cc780f29b68acacc
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-19 19:47:30 +0800

    ocp

--- src/main/java/com/ocp/day10_employees/dao/EmployeeDao.java
+++ src/main/java/com/ocp/day10_employees/dao/EmployeeDao.java
@@ -1,5 +1,18 @@
 package com.ocp.day10_employees.dao;
 
+import com.ocp.day10_employees.entity.Employee;
+import java.util.ArrayList;
+
 public interface EmployeeDao {
-    
+    // CRUD 的規格
+    // 新增員工
+    public void add();
+    // 根據 id 查詢員工
+    public Employee getById(int id);
+    // 查詢所有員工
+    public ArrayList<Employee> employees();
+    // 根據員工 id 來修改薪資
+    public void updateSalaryById(int id, Integer salary);
+    // 根據員工 id 來刪除該名員工
+    public void deleteById(int id);
 }
