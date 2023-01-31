abbc85e27b1af7aa609eda9eed0e8f6fce77709f
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-31 19:12:16 +0800

    ocp

--- src/main/java/com/ocp/day11_inner/inner/Button.java
+++ src/main/java/com/ocp/day11_inner/inner/Button.java
@@ -2,11 +2,17 @@
 package com.ocp.day11_inner.inner;
 // 外部類別
 public class Button {
+    int x = 777;
     // 內部類別
     public class Click {
+        int x = 77;
         // 按下之後要做的事
         public void action() {
+            int x = 7;
             System.out.println("Button 被按了一下");
+            System.out.println(x);
+            System.out.println(this.x);
+            System.out.println(Button.this.x);
         }
     }
 }
