330c3bf4bbe6dae4b701aea4632affb64ec4e6f5
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-31 19:55:43 +0800

    ocp

---
+++ src/main/java/com/ocp/day11_inner/anonymous/DogTest.java
@@ -0,0 +1,23 @@
+package com.ocp.day11_inner.anonymous;
+
+public class DogTest {
+    public static void main(String[] args) {
+        
+        Dog dog = new Dog() {
+            @Override
+            public void eat() {
+                System.out.println("牛肉罐頭");
+            }
+        };
+        dog.eat();
+        
+        Dog dog2 = new Dog() {
+            @Override
+            public void eat() {
+                System.out.println("豬骨頭");
+            }
+        };
+        dog2.eat();
+        
+    }
+}
