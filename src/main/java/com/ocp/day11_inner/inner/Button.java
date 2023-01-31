ac48ac3fd2649723308c7601b45861b40d44723c
MB-teacher <MB-teacher@MB-207-teacher>
2023-01-31 19:16:05 +0800

    ocp

--- src/main/java/com/ocp/day11_inner/inner/Button.java
+++ src/main/java/com/ocp/day11_inner/inner/Button.java
@@ -3,9 +3,11 @@ package com.ocp.day11_inner.inner;
 // 外部類別
 public class Button {
     int x = 777;
+    static int y = 200;
     // 內部類別
     public class Click {
         int x = 77;
+        //static int y = 200; // 內部類別不可以有 static 成員
         // 按下之後要做的事
         public void action() {
             int x = 7;
