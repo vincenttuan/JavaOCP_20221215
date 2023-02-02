83d683de043d942f017af6cc860afd19861b0597
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-02 20:56:41 +0800

    ocp

--- src/main/java/com/ocp/day12_set/DemoSet3.java
+++ src/main/java/com/ocp/day12_set/DemoSet3.java
@@ -1,5 +1,20 @@
 package com.ocp.day12_set;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class DemoSet3 {
-    
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("John", 50000);
+        Employee emp2 = new Employee("Mary", 65000);
+        Employee emp3 = new Employee("Bobo", 80000);
+        Set<Employee> employees = new LinkedHashSet<>();
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+        System.out.println(employees);
+        for(Employee emp : employees) {
+            System.out.printf("姓名: %s 薪資: $%,d\n", emp.getName(), emp.getSalary());
+        }
+    }
 }
