94c01ca6e95c4a4d1debf3b5703922a01884dc29
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-09 20:00:39 +0800

    ocp

--- src/main/java/com/ocp/day14_map/MapDemo2.java
+++ src/main/java/com/ocp/day14_map/MapDemo2.java
@@ -1,5 +1,6 @@
 package com.ocp.day14_map;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -11,6 +12,20 @@ public class MapDemo2 {
         employees.put("Alen", 60000);
         System.out.println(employees);
         // 請計算總分與平均
+        // Java 7
+        Collection<Integer> salary = employees.values();
+        System.out.println(salary);
+        int sum = 0;
+        for(Integer value : salary) {
+            sum += value;
+        }
+        System.out.println(sum);
+        // Java 8 stream
+        int sum2 = employees.values()
+                            .stream()
+                            .mapToInt(Integer::intValue) //.mapToInt(x -> x.intValue())
+                            .sum();
+        System.out.println(sum2);
         
     }
 }
