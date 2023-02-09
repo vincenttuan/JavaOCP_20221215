fc5b50516769ccee373db2c36bd3577e20fe05c3
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-09 18:59:49 +0800

    ocp

--- src/main/java/com/ocp/day14_list/VectorDemo.java
+++ src/main/java/com/ocp/day14_list/VectorDemo.java
@@ -6,11 +6,11 @@ public class VectorDemo {
     public static void main(String[] args) {
         // synchronized 同步集合
         // 支援多執行緒存取
-        Vector v = new Vector();
+        Vector v = new Vector(21, 3);
         System.out.println("size(已用空間數): " + v.size());
         System.out.println("capacity(總空間數): " + v.capacity());
         
-        for(int i=1;i<=9;i++) {
+        for(int i=1;i<=22;i++) {
             v.add(i);
         }
         System.out.println(v);
