eebca0c7ce7e3e5db780f35633d3718ddabb19d2
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-09 21:29:42 +0800

    ocp

---
+++ src/main/java/com/ocp/day14_map/MapDemo4.java
@@ -0,0 +1,20 @@
+package com.ocp.day14_map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo4 {
+    public static void main(String[] args) {
+        Map<String, Integer> scores = new LinkedHashMap<>();
+        scores.put("John", 85);
+        scores.put("Mary", 95);
+        scores.put("Alen", 65);
+        System.out.println(scores);
+        // 請問最高分? 最高分是誰 ?
+        
+        
+        
+        
+        
+    }
+}
