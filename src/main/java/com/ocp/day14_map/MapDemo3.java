f5f349a450973a800fa9fefa9082f20f8f395e31
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-09 20:48:54 +0800

    ocp

---
+++ src/main/java/com/ocp/day14_map/MapDemo3.java
@@ -0,0 +1,17 @@
+package com.ocp.day14_map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo3 {
+    public static void main(String[] args) {
+        Map<String, Double> map = new LinkedHashMap<>();
+        map.put("半徑1", 10.5);
+        map.put("半徑2", 12.0);
+        map.put("半徑3", 7.5);
+        System.out.println(map);
+        // 請計算每一個半徑的圓面積
+        
+        
+    }
+}
