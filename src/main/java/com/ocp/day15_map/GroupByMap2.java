a319af36650adabf65464f2a06c65f1c6c8bc57f
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-14 19:10:10 +0800

    ocp

---
+++ src/main/java/com/ocp/day15_map/GroupByMap2.java
@@ -0,0 +1,13 @@
+package com.ocp.day15_map;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupByMap2 {
+    public static void main(String[] args) {
+        List<String> scores = Arrays.asList("A", "A", "B", "E", "D", "C", "C", "A", "B", "B", "E");
+        System.out.println("全班成績等第: " + scores);
+        // 請寫出每個等第有幾人 ?
+        
+    }
+}
