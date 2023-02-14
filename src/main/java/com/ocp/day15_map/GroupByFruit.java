84a59aecf51fc85c0c72f8fdfc2644f446775203
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-14 20:02:47 +0800

    ocp

---
+++ src/main/java/com/ocp/day15_map/GroupByFruit.java
@@ -0,0 +1,21 @@
+package com.ocp.day15_map;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupByFruit {
+    public static void main(String[] args) {
+        List<Fruit> fruits = Arrays.asList(
+                new Fruit("apple", 10, 9.99),
+                new Fruit("banana", 20, 19.99),
+                new Fruit("orange", 10, 29.99),
+                new Fruit("watermelon", 10, 29.99),
+                new Fruit("papaya", 20, 9.99),
+                new Fruit("apple", 10, 9.99),
+                new Fruit("banana", 10, 19.99),
+                new Fruit("apple", 20, 9.99)
+        );
+        System.out.println(fruits);
+        // 請計算出 19.99=[banana], 29.99=[orange, watermelon], 9.99=[papaya, apple]
+    }
+}
