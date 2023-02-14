129b993aedd250a82b801423f6d141bac0d3d4b7
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-14 19:34:17 +0800

    ocp

--- src/main/java/com/ocp/day15_map/GroupByMap3.java
+++ src/main/java/com/ocp/day15_map/GroupByMap3.java
@@ -2,13 +2,18 @@ package com.ocp.day15_map;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroupByMap3 {
     public static void main(String[] args) {
         List<Integer> scores = Arrays.asList(95, 47, 86, 73, 32);
         System.out.println(scores);
         // 請計算出: 及格=3, 不及格=2
-        
+        Map<String, Long> result =  scores.stream().collect(
+                Collectors.groupingBy(score -> score >= 60 ? "及格" : "不及格", Collectors.counting())
+        );
+        System.out.println(result);
         
     }
 }
