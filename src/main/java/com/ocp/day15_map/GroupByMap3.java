cc401d9e196656d55a667775aa1b74584c46a92b
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-14 19:25:39 +0800

    ocp

---
+++ src/main/java/com/ocp/day15_map/GroupByMap3.java
@@ -0,0 +1,14 @@
+package com.ocp.day15_map;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupByMap3 {
+    public static void main(String[] args) {
+        List<Integer> scores = Arrays.asList(95, 47, 86, 73, 32);
+        System.out.println(scores);
+        // 請計算出: 及格=3, 不及格=2
+        
+        
+    }
+}
