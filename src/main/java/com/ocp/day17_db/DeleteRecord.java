d6591a7570fafad9d113915f6617c995a3b43139
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-21 21:34:20 +0800

    ocp

--- src/main/java/com/ocp/day17_db/DeleteRecord.java
+++ src/main/java/com/ocp/day17_db/DeleteRecord.java
@@ -3,7 +3,6 @@ package com.ocp.day17_db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class DeleteRecord {
