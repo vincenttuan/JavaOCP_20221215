d704b4b5a1f52d2520d5dcc8144da8f8f0ff8b2a
MB-teacher <MB-teacher@MB-207-teacher>
2023-02-23 21:39:11 +0800

    ocp

--- src/main/java/com/ocp/day18_exception/ExceptionDemo4.java
+++ src/main/java/com/ocp/day18_exception/ExceptionDemo4.java
@@ -8,20 +8,30 @@ import java.sql.SQLException;
 
 public class ExceptionDemo4 {
     public static void main(String[] args) {
+        Connection conn = null;
         // 動態載入驅動程式
         try {
             String className = "com.mysql.cj.jdbc.Driver";
             Class.forName(className); // 會拋出 ClassNotFoundException
             // 建立連線
             String url = "jdbc:mysql://localhost:3306/demo?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true";
-            String user = "root";
+            String user = "root1";
             String password = "12345678";
-            Connection conn = DriverManager.getConnection(url, user, password); // 會拋出 SQLException
-            conn.close();
+            conn = DriverManager.getConnection(url, user, password); // 會拋出 SQLException
             System.out.println("任務完成");
         } catch(ClassNotFoundException | SQLException e1) {
             System.out.println("錯誤內容: " + e1);
+            return;
+        } finally { // 保證一定會執行
+            if(conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("連線關閉");
+                } catch(SQLException e2) {
+                    System.out.println("連線關閉失敗");
+                }
+            }
+            System.out.println("程式結束");
         }
-        System.out.println("程式結束");
     }
 }
