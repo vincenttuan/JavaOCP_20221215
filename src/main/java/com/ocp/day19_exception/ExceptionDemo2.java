8fabe7295b3d837d6e917785b9de8b9558c105ed
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-02 19:17:09 +0800

    ocp

---
+++ src/main/java/com/ocp/day19_exception/ExceptionDemo2.java
@@ -0,0 +1,17 @@
+package com.ocp.day19_exception;
+
+public class ExceptionDemo2 {
+    public static void main(String[] args) {
+        boolean isPass = login("admin", "1234");
+        System.out.printf("登入資訊: %b\n", isPass);
+    }
+    
+    // 登入驗證
+    public static boolean login(String username, String password) {
+        if (username.equals("admin") && password.equals("1234")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
