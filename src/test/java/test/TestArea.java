1386dc165dccfd683e387f1f1ae6daae162c6a7f
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-02 21:37:12 +0800

    ocp

--- src/test/java/test/TestArea.java
+++ src/test/java/test/TestArea.java
@@ -10,7 +10,7 @@ import org.junit.Assert;
 // 1. Arrange (安排待測物件)
 // 2. Act (調用測試方法)
 // 3. Assert (驗證是否符合預期結果)
-public class TestTriangleArea {
+public class TestArea {
     @Test
     public void test1() {
         // 1. Arrange (安排待測物件與相關參數)
@@ -20,6 +20,19 @@ public class TestTriangleArea {
         int expected = 25; // 期望結果(自己算的)
         // 2. Act (調用測試方法)
         int actual = calculator.triangleArea(w, h); // 實際運算結果(電腦算的)
+        // 3. Assert (驗證是否符合預期結果)
+        Assert.assertEquals(expected, actual);
+        
+    }
+    @Test
+    public void test2() {
+        // 1. Arrange (安排待測物件與相關參數)
+        Calculator calculator = new Calculator();
+        int w = 10; // 底(寬)
+        int h = 5;  // 高
+        int expected = 30; // 期望結果(自己算的)
+        // 2. Act (調用測試方法)
+        int actual = calculator.rectMeter(w, h); // 實際運算結果(電腦算的)
         // 3. Assert (驗證是否符合預期結果)
         Assert.assertEquals(expected, actual);
         
