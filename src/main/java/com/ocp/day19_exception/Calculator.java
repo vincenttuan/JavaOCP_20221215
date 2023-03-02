82f9a2b11292b3369f044d945ed5b2c7753ca049
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-02 21:11:03 +0800

    ocp

---
+++ src/main/java/com/ocp/day19_exception/Calculator.java
@@ -0,0 +1,9 @@
+package com.ocp.day19_exception;
+
+public class Calculator {
+    // 三角形面積
+    public int triangleArea(int w, int h) {
+        int area = w * h / 2;
+        return area;
+    }
+}
