5535b8877c9d20f6361226233cc6d9f527402281
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-02 19:28:47 +0800

    ocp

---
+++ src/main/java/com/ocp/day19_exception/ExceptionDemo4.java
@@ -0,0 +1,23 @@
+package com.ocp.day19_exception;
+
+public class ExceptionDemo4 {
+    public static void main(String[] args) {
+        boolean isPass = login("admin", "1234");
+        System.out.printf("登入資訊: %b\n", isPass);
+        
+        boolean isPass2 = login("admin2", "2222");
+        System.out.printf("登入資訊: %b\n", isPass2);
+        
+    }
+    
+    // 登入驗證
+    public static boolean login(String username, String password) {
+        if (username.equals("admin") && password.equals("1234")) {
+            return true;
+        } else {
+            // ...
+            RuntimeException e = new RuntimeException("您的使用者名稱或密碼不正確");
+            throw e; // 將例外拋出
+        }
+    }
+}
