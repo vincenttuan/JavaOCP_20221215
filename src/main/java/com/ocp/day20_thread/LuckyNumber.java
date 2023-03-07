3091ddbf260e33f1a15a6d98bfd8f3649827342e
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-07 21:35:00 +0800

    ocp

--- src/main/java/com/ocp/day20_thread/LuckyNumber.java
+++ src/main/java/com/ocp/day20_thread/LuckyNumber.java
@@ -8,7 +8,8 @@ public class LuckyNumber extends Thread {
         String tName = Thread.currentThread().getName();
         for(int i=1;true;i++) {
             int x = (int)(Math.random() * 1000); //0~999
-            System.out.printf("%s 取了第 %d 次得到 %d\n", tName, i, ans);
+            // 印出每一次所取到的資料
+            System.out.printf("%s 取了第 %d 次得到 %d\n", tName, i, x);
             if(x == ans) {
                 System.out.printf("%s 共取了 %d 次才得到 %d\n", tName, i, ans);
                 break;
