824dad6cf5e7c0697a777b996b27f9437d5053b8
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-14 19:20:50 +0800

    ocp

---
+++ src/main/java/com/ocp/day22_thread/Shower.java
@@ -0,0 +1,9 @@
+package com.ocp.day22_thread;
+
+public class Shower {
+    public static void main(String[] args) {
+        Father father = new Father();
+        father.start();
+        
+    }
+}
