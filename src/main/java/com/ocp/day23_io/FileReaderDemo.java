689ef04f2418c97a454eeb040b907887c5f2ca17
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-16 21:00:02 +0800

    ocp

---
+++ src/main/java/com/ocp/day23_io/FileReaderDemo.java
@@ -0,0 +1,32 @@
+package com.ocp.day23_io;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+    // 讀取檔案內容
+    public static void main(String[] args) {
+        File file = new File("src/main/java/com/ocp/day23_io/files/news.txt");
+        FileReader fr = null;
+        try {
+            fr = new FileReader(file); // 開啟檔案, 準備讀取
+            // 逐字讀取
+            System.out.println(fr.read());
+            System.out.println(fr.read());
+            System.out.println(fr.read());
+            System.out.println(fr.read());
+            
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } finally {
+            if(fr != null) {
+                try {
+                    fr.close(); // 關閉檔案
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
+}
