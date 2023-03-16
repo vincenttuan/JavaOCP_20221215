cd4121180e749c618259480f6e37431db8acc95a
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-16 21:27:13 +0800

    ocp

---
+++ src/main/java/com/ocp/day23_io/FileWriterDemo.java
@@ -0,0 +1,10 @@
+package com.ocp.day23_io;
+
+public class FileWriterDemo {
+    // 將資料寫入
+    public static void main(String[] args) {
+        String words = "台積電創辦人張忠謀與「晶片戰爭」作者米勒（Chris Miller）今天在一場近1小時對談中，\n" +
+                       "針對台灣關鍵優勢、晶片製造轉移成本以及國際局勢變化將如何影響半導體產業進行交流與討論。";
+        
+    }
+}
