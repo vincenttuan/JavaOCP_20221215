ff6578f7864b2ada26b05bb7ac850714707dc1bd
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-16 21:31:11 +0800

    ocp

--- src/main/java/com/ocp/day23_io/FileWriterDemo.java
+++ src/main/java/com/ocp/day23_io/FileWriterDemo.java
@@ -3,6 +3,7 @@ package com.ocp.day23_io;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class FileWriterDemo {
     // 將資料寫入
@@ -10,10 +11,9 @@ public class FileWriterDemo {
         String words = "台積電創辦人張忠謀與「晶片戰爭」作者米勒（Chris Miller）今天在一場近1小時對談中，\n" +
                        "針對台灣關鍵優勢、晶片製造轉移成本以及國際局勢變化將如何影響半導體產業進行交流與討論。";
         File file = new File("src/main/java/com/ocp/day23_io/files/news.txt");
-        try(FileWriter fw = new FileWriter(file)) {
-            
+        try(FileWriter fw = new FileWriter(file, Charset.forName("UTF-8"))) {
             fw.write(words); // 將字串資料寫入到檔案中
-            
+            System.out.println("寫檔完成~");
         } catch (IOException e) {
             System.out.println(e);
         }
