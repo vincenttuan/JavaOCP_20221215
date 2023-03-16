9e75de96aed8f1180656681696dcab9bd52e3cb8
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-16 21:11:43 +0800

    ocp

--- src/main/java/com/ocp/day23_io/FileReaderDemo.java
+++ src/main/java/com/ocp/day23_io/FileReaderDemo.java
@@ -1,8 +1,10 @@
 package com.ocp.day23_io;
 
+import com.fasterxml.jackson.core.io.UTF32Reader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class FileReaderDemo {
     // 讀取檔案內容
@@ -10,7 +12,7 @@ public class FileReaderDemo {
         File file = new File("src/main/java/com/ocp/day23_io/files/news.txt");
         FileReader fr = null;
         try {
-            fr = new FileReader(file); // 開啟檔案, 準備讀取
+            fr = new FileReader(file, Charset.forName("UTF-8")); // 開啟檔案, 準備讀取
             // 逐字讀取
             while (true) {                
                 int word = fr.read();
@@ -19,7 +21,6 @@ public class FileReaderDemo {
                 }
                 System.out.print((char)word);
             }
-            
         } catch (IOException ex) {
             System.out.println(ex);
         } finally {
