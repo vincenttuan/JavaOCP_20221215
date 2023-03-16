7192e96076efbb956a741420f69e95ed89df5196
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-16 21:16:47 +0800

    ocp

--- src/main/java/com/ocp/day23_io/FileReaderDemo2.java
+++ src/main/java/com/ocp/day23_io/FileReaderDemo2.java
@@ -5,14 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
-public class FileReaderDemo {
-    // 讀取檔案內容
+public class FileReaderDemo2 {
     public static void main(String[] args) {
         File file = new File("src/main/java/com/ocp/day23_io/files/news.txt");
-        FileReader fr = null;
-        try {
-            fr = new FileReader(file, Charset.forName("UTF-8")); // 開啟檔案, 準備讀取
-            // 逐字讀取
+        try(FileReader fr = new FileReader(file, Charset.forName("UTF-8"))) {
             while (true) {                
                 int word = fr.read();
                 if(word == -1) {
@@ -20,16 +16,8 @@ public class FileReaderDemo {
                 }
                 System.out.print((char)word);
             }
-        } catch (IOException ex) {
+        } catch(IOException ex) {
             System.out.println(ex);
-        } finally {
-            if(fr != null) {
-                try {
-                    fr.close(); // 關閉檔案
-                } catch (IOException ex) {
-                    System.out.println(ex);
-                }
-            }
         }
     }
 }
