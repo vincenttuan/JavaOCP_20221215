e3176bbd4e8c8e6a40548a46d9117577454d3ff6
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-21 19:45:43 +0800

    ocp

--- src/main/java/com/ocp/day24_io/CopyAndSum2.java
+++ src/main/java/com/ocp/day24_io/CopyAndSum2.java
@@ -4,24 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Arrays;
 
-public class CopyAndSum {
+public class CopyAndSum2 {
     public static void main(String[] args) {
-        File source = new File("src/main/java/com/ocp/day24_io/source/score.txt"); // 資料來源
-        File dist = new File("src/main/java/com/ocp/day24_io/dest/score.txt"); // 目的地
+        File source = new File("src/main/java/com/ocp/day24_io/source/score2.txt"); // 資料來源
+        File dist = new File("src/main/java/com/ocp/day24_io/dest/score2.txt"); // 目的地
         
         try(BufferedReader br = new BufferedReader(new FileReader(source));
             FileWriter fw = new FileWriter(dist)){
             
             String line = null;
             while ((line = br.readLine()) != null) { // 逐行讀取, 若讀到 null 表示已經讀完, 跳離迴圈                
-                System.out.print(line);
                 // 透過 split(",") 來拆分字串
                 String[] values = line.split(",");
-                System.out.print(" values[0] = " + values[0]);
-                System.out.print(" values[1] = " + values[1]);
-                int sum = Integer.parseInt(values[0]) + Integer.parseInt(values[1]);
-                System.out.println(" sum = " + sum);
+                int sum = Arrays.stream(values).mapToInt(Integer::parseInt).sum();
                 // 準備待寫入的字串
                 String result = line + "," + sum;
                 // 將字串寫入到檔案中
