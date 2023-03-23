546652ea31a70b7db4eeb6125d49e00f7684c881
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-23 21:22:51 +0800

    ocp

--- src/main/java/io_lab/ReadV30_TSE_2.java
+++ src/main/java/io_lab/ReadV30_TSE_2.java
@@ -23,19 +23,35 @@ public class ReadV30_TSE_2 {
             byte[] block = new byte[blockSize];
             System.arraycopy(bytes, i*blockSize, block, 0, blockSize);
             // 0050  00132950000120900000108850020230322000000  1元大台灣50      00000000000000000010X0            
-            String str = new String(block, charset);
+            //String str = new String(block, charset);
             Map<String, String> map = new LinkedHashMap<>();
-            map.put("STOCK-NO", str.substring(0, 6).trim());
-            map.put("BULL-PRICE", str.substring(6, 15).trim());
-            map.put("LDC-PRICE", str.substring(15, 24).trim());
-            map.put("BEAR-PRICE", str.substring(24, 33).trim());
+            
+            byte[] stockNo = new byte[6];
+            System.arraycopy(block, 0, stockNo, 0, 6);
+            map.put("STOCK-NO", new String(stockNo, charset).trim());
+            
+            byte[] bullPrice = new byte[9];
+            System.arraycopy(block, 6, bullPrice, 0, 9);
+            map.put("BULL-PRICE", new String(bullPrice, charset).trim());
+            
+            byte[] ldcPrice = new byte[9];
+            System.arraycopy(block, 15, ldcPrice, 0, 9);
+            map.put("BULL-PRICE", new String(ldcPrice, charset).trim());
+            
+            byte[] bearPrice = new byte[9];
+            System.arraycopy(block, 24, bearPrice, 0, 9);
+            map.put("BEAR-PRICE", new String(bearPrice, charset).trim());
+            
+            byte[] stockName = new byte[16];
+            System.arraycopy(block, 50, stockName, 0, 16);
+            map.put("STOCK-NAME", new String(stockName, charset).trim());
             
             tseMap.put(map.get("STOCK-NO"), map);
         }
         
         //System.out.println(tseMap);
         long begin = System.nanoTime();
-        System.out.println(tseMap.get("2330").get("LDC-PRICE"));
+        System.out.println(tseMap.get("2330").get("STOCK-NAME"));
         long end = System.nanoTime();
         double time = (end - begin) / Math.pow(10, 9);
         System.out.println("花費時間:" + new DecimalFormat("0.#########").format(time) + " 秒");
