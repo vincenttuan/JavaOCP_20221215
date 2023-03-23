1fa84c9b24116bfbdea83a85c77147306e67284c
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-23 19:30:31 +0800

    ocp

---
+++ src/main/java/io_lab/FileEncodingDetector.java
@@ -0,0 +1,17 @@
+package io_lab;
+
+// 取得檔案的編碼方式
+/*
+<dependency>
+    <groupId>org.mozilla</groupId>
+    <artifactId>juniversalchardet</artifactId>
+    <version>1.0.3</version>
+</dependency>
+*/
+public class FileEncodingDetector {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
