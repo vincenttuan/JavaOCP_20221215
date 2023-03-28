794152f25084da23926c680e350986bfe9b4734f
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-28 21:13:41 +0800

    java

--- src/main/java/com/ocp/day26_socket/web/WebServer.java
+++ src/main/java/com/ocp/day26_socket/web/WebServer.java
@@ -13,41 +13,44 @@ import java.util.Date;
 public class WebServer {
 
     public static void main(String[] args) throws Exception {
-        int port = 5000;
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.err.println("Web Server port : " + port);
+    // création de la socket
+    int port = 5000;
+    ServerSocket serverSocket = new ServerSocket(port);
+    System.err.println("Web Server port : " + port);
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("有 Client 連入");
+    while (true) {
+        // on reste bloqué sur l'attente d'une demande client
+        Socket clientSocket = serverSocket.accept();
+        System.err.println("Waiting");
 
-            // Socket I/O
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-            String s;
-            while ((s = in.readLine()) != null) {
-                System.out.println(s);
-                if (s.isEmpty()) {
-                    break;
-                }
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        
+        // WebServer 接收到瀏覽器的資料
+        String s;
+        while ((s = in.readLine()) != null) {
+            System.out.println(s);
+            if (s.isEmpty()) {
+                break;
             }
-            
-            out.write("HTTP/1.0 200 OK\r\n");
-            out.write("Date: Fri, 31 Dec 2022 23:59:59 GMT\r\n");
-            out.write("Server: Apache/0.8.4\r\n");
-            out.write("Content-Type: text/html\r\n");
-            out.write("Content-Length: 59\r\n");
-            out.write("Expires: Sat, 01 Jan 2022 00:59:59 GMT\r\n");
-            out.write("Last-modified: Fri, 09 Aug 2022 14:21:40 GMT\r\n");
-            out.write("\r\n");
-            out.write("<TITLE>Exemple</TITLE>");
-            out.write("<P>Ceci est une page d'exemple.</P>");
-
-            System.err.println("離開結束");
-            out.close();
-            in.close();
-            clientSocket.close();
         }
+        
+        // WebServer 傳送給瀏覽器的資料
+        out.write("HTTP/1.0 200 OK\r\n");
+        out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+        out.write("Server: Apache/0.8.4\r\n");
+        out.write("Content-Type: text/html\r\n");
+        out.write("Content-Length: 59\r\n");
+        out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+        out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+        out.write("\r\n");
+        out.write("<TITLE>Exemple</TITLE>");
+        out.write("<P>現在時間: " + new Date() + "</P>");
+
+        System.out.println("連線結束");
+        out.close();
+        in.close();
+        clientSocket.close();
     }
+}
 }
