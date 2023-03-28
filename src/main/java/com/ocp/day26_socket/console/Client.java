33d56f7b4abf2a7fd5b2f13ae55c52a19bc52366
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-28 20:31:51 +0800

    java

---
+++ src/main/java/com/ocp/day26_socket/console/Client.java
@@ -0,0 +1,34 @@
+package com.ocp.day26_socket.console;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("請輸入Server IP(localhost): ");
+        String serverIP = scanner.next();
+        // 1. 建立 Socket 連線
+        Socket socket = new Socket(serverIP, 5555);
+        // 2. 建立 Socket I/O
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        while (true) {            
+            // 3. 輸入要傳送的訊息
+            System.out.println("請輸入要傳送的訊息: ");
+            String message = scanner.next();
+            // 4. 訊息送出
+            oos.writeObject(message);
+            // 5. 是否離開
+            if(message.equalsIgnoreCase("exit")) {
+                socket.close(); // socket 關閉
+                break;
+            }
+        }
+        System.out.println("Socket 關閉");
+    }
+    
+    
+}
