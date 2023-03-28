2cd38a58e7707bb2fa185f6d24602deacf5edcf5
MB-teacher <MB-teacher@MB-207-teacher>
2023-03-28 21:07:25 +0800

    java

--- src/main/java/com/ocp/day26_socket/web/WebServer.java
+++ src/main/java/com/ocp/day26_socket/web/WebServer.java
@@ -1,7 +1,6 @@
 package com.ocp.day26_socket.web;
 
 // 利用 Socket 撰寫一個 WebServer
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -12,35 +11,43 @@ import java.net.Socket;
 import java.util.Date;
 
 public class WebServer {
+
     public static void main(String[] args) throws Exception {
-        WebServer webServer = new WebServer();
-        webServer.start();
-    }
-    
-    private void start() throws Exception {
-        ServerSocket server;
-        System.out.println("WebServer 啟動 on port 5000");
-        server = new ServerSocket(5000);
-        System.out.println("等待連線");
+        int port = 5000;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.err.println("Web Server port : " + port);
+
         while (true) {
-            Socket socket = server.accept();
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "big5")), true);
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("有 Client 連入");
+
+            // Socket I/O
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String s;
+            while ((s = in.readLine()) != null) {
+                System.out.println(s);
+                if (s.isEmpty()) {
+                    break;
+                }
+            }
             
-            System.out.println("瀏覽器已經連進來了");
-            System.out.println("Client data: " + br.readLine());
-            
-            // 回應 HTTP 文件結構
-            // Part I HTTP 初始列
-            out.println("HTTP/1.1 200 OK");
-            // Part II HTTP 標頭
-            out.println("Content-Type: text/html");
-            out.println("Server: SocketWebServer");
-            // 空白列
-            System.out.println("");
-            // Part III 內文本體
-            out.println("<html><h1>現在時刻: " + new Date() + " </h1></html>");
-            
+            out.write("HTTP/1.0 200 OK\r\n");
+            out.write("Date: Fri, 31 Dec 2022 23:59:59 GMT\r\n");
+            out.write("Server: Apache/0.8.4\r\n");
+            out.write("Content-Type: text/html\r\n");
+            out.write("Content-Length: 59\r\n");
+            out.write("Expires: Sat, 01 Jan 2022 00:59:59 GMT\r\n");
+            out.write("Last-modified: Fri, 09 Aug 2022 14:21:40 GMT\r\n");
+            out.write("\r\n");
+            out.write("<TITLE>Exemple</TITLE>");
+            out.write("<P>Ceci est une page d'exemple.</P>");
+
+            System.err.println("離開結束");
+            out.close();
+            in.close();
+            clientSocket.close();
         }
     }
 }
